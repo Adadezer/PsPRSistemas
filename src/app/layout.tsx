0cde6e552a5e868c306021b548a8bc0073217c2d
adadezer <adadezer@gmail.com>
2023-11-14 12:33:31 -0300

    perf: :zap: change location header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Poppins } from 'next/font/google'
 import TailwindMaterialProvider from '@/providers/tailwindMaterial'
-import Header from './components/Header'
+import Header from '../components/Header'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800', '900'] })
 
