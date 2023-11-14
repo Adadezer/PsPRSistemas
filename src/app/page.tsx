b5b8bab83f772de32b0e99bb0d34dc7be74eff32
adadezer <adadezer@gmail.com>
2023-11-13 21:12:21 -0300

    feat: :sparkles: search bar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,7 @@
-"use client"
-
-import Image from 'next/image'
-import { Button, Input } from '@material-tailwind/react'
+import SearchBar from './components/SearchBar'
 
 export default function Home() {
   return (
-    <>
-      <h1 className='text-primary'>Meu titulo</h1>
-      <div className="relative flex w-full gap-2 md:w-max">
-        <Input
-          type="search"
-          color="white"
-          label="Pesquise um contato"
-          className="pr-20"
-          containerProps={{ className: "min-w-[288px]"}}
-          crossOrigin={undefined}
-        />
-        <Button
-          size="sm"
-          className="!absolute right-1 top-1 rounded bg-primary"
-        >
-          Search
-        </Button>
-      </div>
-      <Button>Button</Button>
-    </>
-
+   <SearchBar />
   )
 }
