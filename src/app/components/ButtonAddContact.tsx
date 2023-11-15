564a1642e0083aa042043d4e9a554125c3eb8b59
adadezer <adadezer@gmail.com>
2023-11-15 19:36:17 -0300

    feat: :sparkles: add form add contact

--- src/app/components/ButtonAddContact.tsx
+++ src/app/components/ButtonAddContact.tsx
@@ -1,28 +1,31 @@
 'use client'
 
 import { Button } from '@material-tailwind/react'
+import Link from 'next/link'
 import React from 'react'
 
 function ButtonAddContact() {
   return (
     <div className="continer mx-auto flex justify-center mt-5">
-      <div className="flex flex-col items-center">
-        <Button color="green" className="px-7">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </Button>
-        <p>Adicionar contato</p>
-      </div>
+      <Link href={"/form/AddContact"}>
+        <div className="flex flex-col items-center">
+          <Button color="green" className="px-7">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </Button>
+          <p>Adicionar contato</p>
+        </div>
+      </Link>
     </div>
   );
 }
---
+++ src/app/form/AddContact/page.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import React from "react"
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Card, Input, Button, Typography } from "@material-tailwind/react";
+
+const AddContactSchema = yup.object().shape({
+  name: yup.string().required("campo nome é obrigatório"),
+  nickname: yup.string(),
+  cpf: yup.string(),
+  phone: yup.string().required("campo telefone é obrigatório"),
+  email: yup.string().email()
+});
+
+function AddContact() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(AddContactSchema)
+  });
+
+  const onSubmit = (data: any) => { alert(JSON.stringify(data)) };
+
+  return (
+    <div className="container mx-auto my-7 lg:w-1/2">
+      <Card className="items-center bg-secondary shadow-sm shadow-gray-800 py-14 mx-2 lg:mt-0">
+        <Typography variant="h3" className="text-primary">
+          Adicione um contato
+        </Typography>
+
+        <Typography className="mt-1 mx-2 lg:mx-0 font-normal text-center text-gray-600">
+          Preencha os campos e depois clique em adicionar contato
+        </Typography>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-8 mb-2 w-64 sm:w-96">
+          <div className="mb-1 flex flex-col gap-4">
+            <div className="bg-transparent">
+              <Input {...register("name")} size="lg" label="Nome" color="white" crossOrigin={undefined} />
+              {errors.name && <p className="text-red-300 pl-2 text-sm">{errors.name.message}</p>}
+            </div>
+            <div>
+              <Input {...register("nickname")} size="lg" label="Apelido" color="white" crossOrigin={undefined} />
+              {errors.nickname && <p>{errors.nickname.message}</p>}
+            </div>
+            <div>
+              <Input {...register("cpf")} size="lg" label="CPF" color="white" crossOrigin={undefined} />
+              {errors.cpf && <p>{errors.cpf.message}</p>}
+            </div>
+            <div>
+              <Input {...register("phone")} size="lg" label="Telefone" color="white" crossOrigin={undefined} type="number" />
+              {errors.phone && <p className="text-red-300 pl-2 text-sm">{errors.phone.message}</p>}
+            </div>
+            <div>
+              <Input {...register("email")} size="lg" label="Email" color="white" crossOrigin={undefined} />
+              {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <Button className="mt-10" fullWidth color="green" type="submit"> Adicionar contato</Button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+}
+
+export default AddContact
