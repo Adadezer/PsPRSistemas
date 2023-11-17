0a5532766d609847fe5cc8233f1a6f26a9af97c5
adadezer <adadezer@gmail.com>
2023-11-17 16:53:11 -0300

    feat: :sparkles: logic pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-import { Pagination } from '@/components/Pagination'
 import CardContact from './components/CardContact'
 import ButtonAddContact from './components/ButtonAddContact'
 import SearchBar from './components/SearchBar'
 
-export default function Home() {
+export type PageProps = {
+	params: { [key: string]: string | string[] | undefined };
+	searchParams?: { [key: string]: string | string[] | undefined };
+};
+
+export default function Home(props: PageProps) {
   return (
     <>
       <SearchBar />
       <ButtonAddContact />
-      <CardContact />
-      <Pagination />
+      <CardContact {...props} />
     </>
   )
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,59 +1,78 @@
-"use client"
-
 import React from "react";
-import { Button, IconButton } from "@material-tailwind/react";
-import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { ArrowLongRightIcon, ArrowLongLeftIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { cn } from "@/lib/cn";
+
+type PaginationProps = {
+	page?: string;
+	totalPages: number;
+	hasNextPage: boolean;
+};
  
-export function Pagination() {
-  const [active, setActive] = React.useState(1);
- 
-  const getItemProps = (index: React.SetStateAction<number>) =>
-    ({
-      variant: active === index ? "filled" : "text",
-      color: "black",
-      onClick: () => setActive(index),
-      className: "text-white"
-    } as any);
- 
-  const next = () => {
-    if (active === 5) return;
- 
-    setActive(active + 1);
-  };
- 
-  const prev = () => {
-    if (active === 1) return;
- 
-    setActive(active - 1);
-  };
+export function Pagination(props: PaginationProps) {
+  const { page = 1, totalPages, hasNextPage } = props;
+  
+  const currentPage = Math.min(Math.max(Number(page), 1), totalPages);
+  
+  const getPagesToShow = () => {
+		let startPage = currentPage - 2;
+		let endPage = currentPage + 2;
+
+		if (currentPage <= 3) {
+			startPage = 1;
+			endPage = totalPages;
+		} else if (currentPage >= totalPages - 2) {
+			startPage = totalPages - 4;
+			endPage = totalPages;
+		}
+
+		return Array.from(
+			{ length: endPage - startPage + 1 },
+			(_, i) => startPage + i,
+		);
+	};
+
+  const pages = getPagesToShow();
  
   return (
     <div className="container mx-auto flex justify-center gap-4 mt-12 mb-7">
-      <Button
-        variant="text"
-        className="flex items-center gap-2 bg-primary"
-        color="white"
-        onClick={prev}
-        disabled={active === 1}
-      >
-        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
-      </Button>
-      <div className="flex items-center gap-2">
-        <IconButton {...getItemProps(1)}>1</IconButton>
-        <IconButton {...getItemProps(2)}>2</IconButton>
-        <IconButton {...getItemProps(3)}>3</IconButton>
-        <IconButton {...getItemProps(4)}>4</IconButton>
-        <IconButton {...getItemProps(5)}>5</IconButton>
-      </div>
-      <Button
-        variant="text"
-        className="flex items-center gap-2 bg-primary"
-        color="white"
-        onClick={next}
-        disabled={active === 5}
-      >
-        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-      </Button>
-    </div>
+			<Link
+				className={cn(
+					'flex items-center gap-2 bg-primary rounded py-2 px-7',
+					currentPage === 1 ? 'pointer-events-none bg-red-200' : '',
+				)}
+				href={`?page=${currentPage - 1}`}
+			>
+				<ArrowLongLeftIcon strokeWidth={2} className="h-6 w-6" />
+			</Link>
+
+			<nav
+				aria-label="Pagination"
+				className="flex items-center gap-2"
+			>
+				{pages.map((p) => (
+					<Link
+						key={p}
+						className={cn(
+							'relative inline-flex items-center bg-secondary rounded px-5 py-3 text-sm font-medium hover:bg-gray-700 mx-1',
+							p === currentPage ? 'pointer-events-none bg-gray-800' : '',
+						)}
+						href={`?page=${p}`}
+					>
+						{p}
+					</Link>
+				))}
+			</nav>
+
+			<Link
+				className={cn(
+					'flex items-center gap-2 bg-primary rounded py-2 px-7',
+					!hasNextPage ? 'pointer-events-none bg-red-200' : '',
+				)}
+				href={`?page=${currentPage + 1}`}
+			>
+				<ArrowLongRightIcon strokeWidth={2} className="h-6 w-6" />
+			</Link>
+		</div>
   );
 }
