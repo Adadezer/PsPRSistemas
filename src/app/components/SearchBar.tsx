13dd2b252c0506c4c86fab6cc41dcb175f8a8bf9
adadezer <adadezer@gmail.com>
2023-11-18 23:17:36 -0300

    refactor: :recycle: change button search

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { Input } from '@material-tailwind/react'
+import { Input } from '@material-tailwind/react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -19,29 +19,29 @@ function SearchBar() {
     search: string;
   }
 
-  const { handleSubmit, register, formState: {errors} } = useForm<ContactSearchForm>({
-    resolver: yupResolver(SearchContactSchema)
+  const { handleSubmit, register, formState: { errors } } = useForm<ContactSearchForm>({
+    resolver: yupResolver(SearchContactSchema),
   });
 
   const onSubmit = (data: ContactSearchForm) => {
     router.push(`/search?search=${data.search}`);
-  }
+  };
 
   return (
-    <div className="container mx-auto relative w-1/2 mt-5">
-      <div className="flex gap-2 items-center justify-center">
-        <Input
-          size="lg"
+    <div className="container mx-auto w-3/4 lg:w-1/2 mt-5">
+       <div className="relative gap-2">
+        <Input size="lg"
           color="white"
           label="Digite um contato"
-          containerProps={{ className: "min-w-[289px]" }}
+          containerProps={{ className: "min-w-0" }}
           crossOrigin={undefined}
           {...register("search")}
         />
-        <div onClick={() => handleSubmit(onSubmit)()} className="bg-primary py-2.5 px-5 rounded-md cursor-pointer">
-          <MagnifyingGlassIcon className="h-6 w-6" />
-        </div>
-      </div>
+        <MagnifyingGlassIcon
+          onClick={() => handleSubmit(onSubmit)()}
+          className="h-6 w-6 cursor-pointer !absolute right-2 top-2 text-gray-500"
+        />
+       </div>
       {errors.search && (<p className="text-red-300 text-sm">{errors.search.message}</p>)}
     </div>
   );
